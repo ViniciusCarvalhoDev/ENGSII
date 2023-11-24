902d0ac1c7ba2a14720ef332100fa475c4a5402c
Vinicius Carvalho <viniciusgabr@gmail.com>
2023-11-24 20:08:54 -0300

    Changing structure

---
+++ tJava/src/main/java/com/exemple/main.java
@@ -0,0 +1,35 @@
+package main.java.com.exemple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Utils.MySet;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		MySet mySet = new MySet();
+        
+        List<Object> listA = new ArrayList<>();
+        listA.add(1);
+        listA.add(2);
+        listA.add(3);
+        listA.add(25);
+        
+        mySet.addAll(listA);
+        mySet.uniaoConj(listA);
+
+
+        List<Object> listB = new ArrayList<>();
+        listB.add(3);
+        listB.add(4);
+        listB.add(5);
+
+        List<Object> result = mySet.uniaoConj(listB);
+        System.out.println("Union: " + result);
+        
+        System.out.println("Union: " + mySet.intersConj(listB));
+	}
+
+}
