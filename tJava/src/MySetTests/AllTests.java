8cd33b1c11134c3abea4b646a0db8b743c7bb76a
Vinicius Carvalho <viniciusgabr@gmail.com>
2023-11-24 18:44:05 -0300

    changng unit tests

--- tJava/src/MySetTests/AllTests.java
+++ tJava/src/MySetTests/AllTests.java
@@ -79,6 +79,6 @@ class AllTests {
 
         List<Integer> result = setA.diffConj(setB.getConjunto());
 
-        assertEquals(Arrays.asList(1, 2, 3), result);
+        assertEquals(Arrays.asList(1), result);
     }
 }
